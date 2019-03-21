b8cc16afe570ae5ba9e1f2a7fcc8d6d7272c9ba4
Caroline Lemieux <clemieux@cs.berkeley.edu>
2019-03-20 18:02:19 -0700

    Trust that the environment variables will be passed with export?

--- libarchive-2017-01-04/build-diff.sh
+++ libarchive-2017-01-04/build-diff.sh
@@ -13,7 +13,7 @@ build_lib() {
   rm -rf BUILD
   cp -rf SRC BUILD
   (cd BUILD && patch -p1 < ../cur_diff.patch)
-  (cd BUILD/build && ./autogen.sh && cd .. && ./configure --without-nettle WAYPOINTS="diff" CFLAGS="$CFLAGS" CXXFLAGS="$CXXFLAGS" && make -j $JOBS)
+  (cd BUILD/build && ./autogen.sh && cd .. && ./configure --without-nettle && make -j $JOBS)
 }
 
 get_git_revision https://github.com/libarchive/libarchive.git 51d7afd3644fdad725dd8faa7606b864fd125f88 SRC
