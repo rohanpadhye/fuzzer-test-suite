528f641a8bef8dccfb6a6b56b4fcb11fec0e0761
Rohan Padhye <rohanpadhye@cs.berkeley.edu>
2019-03-22 15:18:05 -0700

    libxml: Add target for validity

---
+++ libxml2-v2.9.2/target-valid.cc
@@ -0,0 +1,26 @@
+// Copyright 2016 Google Inc. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+#include <string>
+#include <vector>
+#include "libxml/xmlversion.h"
+#include "libxml/parser.h"
+#include "libxml/HTMLparser.h"
+#include "libxml/tree.h"
+
+#include "afl_valid.h"
+
+void invalid (void * ctx, const char * msg, ...) {
+  va_list args;
+  va_start(args, msg);
+  vfprintf(stderr, msg, args);
+  va_end(args);
+  __afl_valid_assumption_failure();
+}
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+  xmlSetGenericErrorFunc(NULL, &invalid);
+  if (auto doc = xmlReadMemory(reinterpret_cast<const char *>(data), size,
+                               "noname.xml", NULL, 0))
+    xmlFreeDoc(doc);
+  return 0;
+}
