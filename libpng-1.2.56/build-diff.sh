b2c842c83318f31f7e054d9f233297a686a62bab
Caroline Lemieux <clemieux@cs.berkeley.edu>
2019-03-22 13:18:45 -0700

    diff things for png

---
+++ libpng-1.2.56/build-diff.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Copyright 2016 Google Inc. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+. $(dirname $0)/../custom-build.sh $1 $2
+. $(dirname $0)/../common.sh
+
+[ ! -e libpng-1.2.56.tar.gz ] && wget https://vorboss.dl.sourceforge.net/project/libpng/libpng12/older-releases/1.2.56/libpng-1.2.56.tar.gz
+[ ! -e libpng-1.2.56 ] && tar xf libpng-1.2.56.tar.gz
+
+build_fuzzer || exit 1
+
+if [ -z $DISABLE_TARGET_ARG ]; then
+        export CFLAGS="-target_locations=$(dirname $0)/cur_targets.txt $CFLAGS"
+        export CXXFLAGS="-target_locations=$(dirname $0)/cur_targets.txt $CXXFLAGS"
+fi
+
+build_lib() {
+  rm -rf BUILD
+  cp -rf libpng-1.2.56 BUILD
+  (cd BUILD && patch -p1 < ../cur_diff.patch)
+  (cd BUILD && ./configure &&  make -j)
+}
+
+cp $(dirname $0)/cur_diff.patch .
+build_lib || exit 1
+if [[ $FUZZING_ENGINE == "hooks" ]]; then
+  # Link ASan runtime so we can hook memcmp et al.
+  LIB_FUZZING_ENGINE="$LIB_FUZZING_ENGINE -fsanitize=address"
+fi
+set -x
+$CXX $CXXFLAGS -std=c++11 $SCRIPT_DIR/target.cc BUILD/.libs/libpng12.a $LIB_FUZZING_ENGINE -I BUILD/ -I BUILD -lz -o $EXECUTABLE_NAME_BASE
+$CXX $CXXFLAGS -std=c++11 $SCRIPT_DIR/target.cc BUILD/.libs/libpng12.a $LIB_FUZZING_ENGINE -I BUILD/ -I BUILD -lz -o $EXECUTABLE_NAME_BASE-structure-aware \
+  -include $SCRIPT_DIR/png_mutator.h -DPNG_MUTATOR_DEFINE_LIBFUZZER_CUSTOM_MUTATOR
