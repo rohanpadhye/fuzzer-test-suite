e8d2bf7b5bdb9af7f6f82f27dfc35c9cd2c42849
Rohan Padhye <rohanpadhye@cs.berkeley.edu>
2019-04-03 09:59:46 -0700

    Xml: Print assumption failure

--- libxml2-v2.9.2/target-valid.cc
+++ libxml2-v2.9.2/target-valid.cc
@@ -9,18 +9,22 @@
 
 #include "afl_valid.h"
 
+static bool errors;
+
 void invalid (void * ctx, const char * msg, ...) {
   va_list args;
   va_start(args, msg);
   vfprintf(stderr, msg, args);
   va_end(args);
-  __afl_valid_assumption_failure();
+  errors = true;
 }
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+  errors = false;
   xmlSetGenericErrorFunc(NULL, &invalid);
   if (auto doc = xmlReadMemory(reinterpret_cast<const char *>(data), size,
                                "noname.xml", NULL, 0))
     xmlFreeDoc(doc);
+  ASSUME1(!errors);
   return 0;
 }
